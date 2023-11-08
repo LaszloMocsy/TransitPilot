e9715b6fb353efca2695a89bcc381e3e5f90f338
László Mócsy <laszlomocsy.dev@gmail.com>
2023-11-08 13:53:35 +0100

    Remove previous idea

--- route.h
+++ route.h
@@ -4,17 +4,6 @@
 #include "tstop.h"
 #include "tline.h"
 
-typedef struct Distance_t {
-    int stopFrom, stopTo, line, travelTime;
-    struct Distance_t *next;
-} Distance;
-
-static int *GetSameLines(TStop *stopA, TStop *stopB);
-
-static Distance *GetDistanceBetweenTwoStops(TStopsArray *stops, TLinesArray *lines, int stopA_id, int stopB_id);
-
 void PlanRoute(TStopsArray *stops, TLinesArray *lines, int stopA_id, int stopB_id);
-
-void Distance_free(Distance *head);
 
 #endif //TRANSITPILOT_ROUTE_H

5b66730732f98abf3d1298dbae486807672ba0a6
László Mócsy <laszlomocsy.dev@gmail.com>
2023-11-08 14:03:10 +0100

    Create a basic `isOnSameLine` function

--- route.c
+++ route.c
@@ -1,5 +1,19 @@
 #include "route.h"
 
+/// Check if the two given stops are on tha same line
+/// \param stopA <c>TStop</c> from all the stops
+/// \param stopB Second <c>TStop</c> from all the stops
+/// \return <c>true</c>, if the two stops are on the same line, otherwise <c>false</c>
+bool IsOnSameLine(TStop stopA, TStop stopB) {
+    for (int i = 0; i < stopA.transferCount; ++i) {
+        for (int j = 0; j < stopB.transferCount; ++j) {
+            if (stopA.transfers[i] == stopB.transfers[j])
+                return true;
+        }
+    }
+    return false;
+}
+
 /// Plans a rout from stop A to B and returns the route's specs
 /// \param stops The array that contains all <c>TStop</c>s
 /// \param lines The array that contains all <c>TLine</c>s
@@ -13,4 +27,8 @@ void PlanRoute(TStopsArray *stops, TLinesArray *lines, int stopA_id, int stopB_i
         printf("Stop A and B cannot be the same!\n\n");
         return;
     }
+
+    bool isOnSameLine = IsOnSameLine(*stops->items[stopA_id], *stops->items[stopB_id]);
+    printf("\nThe two stops are %s\n\n", isOnSameLine ? "on the same line!" : "NOT on the same line!");
+    //FIXME: Create new function for checking if the two stops are on the same line
 }
--- route.h
+++ route.h
@@ -4,6 +4,10 @@
 #include "tstop.h"
 #include "tline.h"
 
+typedef struct Route_t {
+    int *stops, *lines;
+} Route;
+
 void PlanRoute(TStopsArray *stops, TLinesArray *lines, int stopA_id, int stopB_id);
 
 #endif //TRANSITPILOT_ROUTE_H
